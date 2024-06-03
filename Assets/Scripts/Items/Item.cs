5613e42072ed8cf112f8501710466382532ee6da
Slavathehiller <hiller@tut.by>
2024-06-03 20:33:24 +0300

    Обновление текстур

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-public abstract class Item
+﻿public abstract class Item
 {
+    public string Name { get; set; }
+    public string Description { get; set; }
     public float Weight {  get; set; }
     public float Volume {  get; set; }
 }
