d2b4b5ea0889b3fbb9f0034553e4742c23b372d0
Slavathehiller <hiller@tut.by>
2024-06-03 22:29:44 +0300

    Обновление всего

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,7 +1,11 @@
-﻿public abstract class Item
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class Item
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
     public float Weight {  get; set; }
     public float Volume {  get; set; }
 }
