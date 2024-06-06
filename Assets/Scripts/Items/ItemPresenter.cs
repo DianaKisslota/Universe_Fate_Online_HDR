46f9f81634e7f61e1ee54393bebe6eee4796233c
Slavathehiller <hiller@tut.by>
2024-06-06 22:04:06 +0300

    Таскание предметов в инвентаре

--- Assets/Scripts/Items/ItemPresenter.cs
+++ Assets/Scripts/Items/ItemPresenter.cs
@@ -1,11 +1,17 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ItemPresenter : MonoBehaviour
+public class ItemPresenter : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     [SerializeField] private Image _icon;
     [SerializeField] private TMP_Text _countText;
+
+    private Transform _transportPanel;
+    private Transform _oldParent;
+    protected CanvasGroup _canvasGroup;
+    protected RectTransform _rectTransform;
 
     private int _count;
     public Item Item { get; set; }
@@ -19,10 +25,39 @@ public class ItemPresenter : MonoBehaviour
         set
         {
             _count = value;
-            if (value > 0)
+            if (value > 1)
                 _countText.text = value.ToString();
             else
                 _countText.text = string.Empty;
         }
+    }
+
+    private void Start()
+    {
+        _transportPanel = GameObject.Find("TransportPanel").transform;
+        _canvasGroup = gameObject.AddComponent<CanvasGroup>();
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _canvasGroup.blocksRaycasts = false;
+        _oldParent = transform.parent;
+        transform.SetParent(_transportPanel);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        _rectTransform.anchoredPosition += eventData.delta;// / _mainCanvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (transform.parent == _transportPanel)
+        {
+            transform.SetParent(_oldParent);
+            transform.localPosition = Vector2.zero;
+        }
+        _canvasGroup.blocksRaycasts = true;
     }
 }
