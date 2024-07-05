74560e50d9cc1dcc37cfea3740884d79d7c21501
vitia88 <vitia88@inbox.ru>
2024-07-05 21:06:51 +0300

    Interface
    повторная отправка

---
+++ Assets/Interface_ UI/Interface_NEW/Interface/Script/PopupController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class PopupController : MonoBehaviour
+{
+    public GameObject popupWindow;
+    public GameObject staticWindow;
+    public AudioSource popupSound;
+
+    // ¬ызываетс€ при нажатии на UI кнопку дл€ открыти€ или закрыти€ всплывающего окна
+    public void TogglePopup()
+    {
+        if (popupWindow != null)
+        {
+            bool newState = !popupWindow.activeSelf; // Ќовое состо€ние всплывающего окна
+
+            popupWindow.SetActive(newState);
+
+            // ≈сли всплывающее окно открыто, закрываем статическое окно
+            if (newState && staticWindow != null)
+            {
+                staticWindow.SetActive(false);
+            }
+            // ≈сли всплывающее окно закрыто, открываем статическое окно
+            else if (!newState && staticWindow != null)
+            {
+                staticWindow.SetActive(true);
+            }
+
+            // ¬оспроизводим звук при открытии всплывающего окна
+            if (newState && popupSound != null)
+            {
+                popupSound.Play();
+            }
+        }
+    }
+
+    void Update()
+    {
+        // ѕровер€ем нажатие на клавишу Esc
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // ≈сли всплывающее окно открыто, закрываем его
+            if (popupWindow.activeSelf)
+            {
+                TogglePopup();
+            }
+            // ≈сли всплывающее окно закрыто, открываем его
+            else
+            {
+                TogglePopup();
+            }
+        }
+    }
+}
