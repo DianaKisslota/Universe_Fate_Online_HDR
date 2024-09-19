a7f74cbcf20d3972e4bb917cb10cf7784fa09d0b
Slavathehiller <hiller@tut.by>
2024-09-19 14:10:13 +0300

    Починка всех сцен

--- Assets/Scripts/DataSource/SectorData/MainMapSectors/Global_Map_A/SectorMain1111.cs
+++ Assets/Scripts/DataSource/SectorData/MainMapSectors/Global_Map_A/SectorMain1111.cs
@@ -2,5 +2,8 @@ public class SectorMain1111 : SectorData
 {
     public SectorMain1111(int x, int y) : base("Main", x, y)
     {
+        AddMonster(new EntitySpawner(typeof(FeralDog), 2, 4));
+        AddBattleScene("BattleScene_forest_1");
+        AddBattleScene("BattleScene_forest_2");
     }
 }
